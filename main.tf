9e0dea4d2c39537e2790acb3dde91959f9f2123a
sandeepredy <sandeepredy.ch@gmail.com>
2023-04-11 07:41:07 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+#setting-up provider
+
+provider "aws" {
+    region = "us-east-1"  
+}
+
+# write the ec2 instance config 
+resource "aws_instance" "ec2_public" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    security_groups = ["${aws_security_group.sandeep-ssh-security-group.id}"]
+    subnet_id = aws_subnet.public-subnet-1.id
+    associate_public_ip_address = true
+    lifecycle {
+      create_before_destroy = true
+    }
+    tags ={
+        Name = "sandeep-ec2-Public"
+    }
+
+  
+}
