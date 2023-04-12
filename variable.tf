8c2cef8eee3f0952c0de4cd003bef6a290bce8fa
sandeepredy <sandeepredy.ch@gmail.com>
2023-04-12 05:56:24 +0000

    variables updated

--- variable.tf
+++ variable.tf
@@ -8,8 +8,15 @@ variable "ami_id" {
 }
 
 variable "security_group" {
-    default = ""
+    default = "nishant-ssh-security-group"
 }
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "key_name" {
+    default = "nishant-keypair-terraform"
+}
+
 
 
 variable "Public_Subnet_1" {
